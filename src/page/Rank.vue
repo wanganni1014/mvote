6c19f3ba08008b674cfed3f0101a7ea350c2508e
yank <1210912570@qq.com>
2021-01-28 16:00:13 +0800

    更新Rank

--- src/page/Rank.vue
+++ src/page/Rank.vue
@@ -1,14 +1,131 @@
 <template>
   <div>
-    排名
+    <div class="sort-bg">
+      <span
+        class="sort-item"
+        :class="sortIndex == index ? 'sort-item-select' : ''"
+        v-for="(sort, index) in sorts"
+        :key="'sort' + index"
+        @click.stop="onSortItemTap(index)"
+        >{{ sort }}</span
+      >
+    </div>
+    <div class="rank-title">仅显示前100名</div>
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <div class="rank-item" v-for="(user, index) in list" :key="'rank' + index">
+        <span class="index">{{ index + 1 }}</span>
+        <img class="avatar" src="" alt="" />
+        <span class="order">123</span>
+        <span class="name">哈哈哈哈</span>
+        <span class="piao">123123</span>
+      </div>
+    </van-list>
     <Tabbar />
   </div>
 </template>
 
 <script>
-import Tabbar from '@/components/Tabbar.vue'
+import Tabbar from "@/components/Tabbar.vue";
+import Vue from "vue";
+import { Cell, CellGroup } from "vant";
+
+Vue.use(Cell);
+Vue.use(CellGroup);
+
 export default {
-  name: 'Rank',
-  components: {Tabbar}
-}
+  name: "Rank",
+  components: { Tabbar },
+  data() {
+    return {
+      list: [1, 1, 1, 1, 1, 1],
+      sortIndex: 0,
+      sorts: ["全部", "广播体操", "武术", "健身操", "仰卧起坐", "太极拳", "跳绳"],
+      loading: false,
+      finished: true,
+    };
+  },
+
+  methods: {
+    onLoad() {},
+
+    onSortItemTap(index) {
+      if (this.sortIndex != index) {
+        this.sortIndex = index;
+      }
+    },
+  },
+};
 </script>
+
+<style>
+.sort-bg {
+  width: 100%;
+  padding: 10px 14px 0 8px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: wrap;
+}
+.sort-item {
+  border: solid 1px #dfdfdf;
+  font-size: 14px;
+  line-height: 14px;
+  padding: 6px 10px;
+  text-align: center;
+  min-width: 40px;
+  background-color: white;
+  color: #888;
+  border-radius: 3px;
+  margin-left: 6px;
+}
+
+.sort-item-select {
+  background-color: rgb(233, 181, 85);
+  color: white;
+}
+
+.rank-title {
+  padding: 15px 0;
+  color: #666;
+}
+.rank-item {
+  width: 100%;
+  padding: 18px 14px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  align-content: center;
+  border-top: solid 1px #dfdfdf;
+}
+.rank-item .index {
+  width: 50px;
+}
+.rank-item .avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: solid 1px #dedede;
+}
+.rank-item .order {
+  width: 80px;
+  text-align: center;
+}
+.rank-item .name {
+  flex-grow: 1;
+  text-align: left;
+}
+.rank-item .piao {
+  color: rgb(230, 147, 47);
+  font-weight: bold;
+  margin-left: 15px;
+}
+</style>
